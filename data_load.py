fd5359d2314bc2b8563bfd1d0d3e4d4837af0c7f
amoriqbal <amoriqbalmail@gmail.com>
2026-03-08 13:45:49 +0000

    refining

--- data_load.py
+++ data_load.py
@@ -67,12 +67,12 @@ class Normalize(object):
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         
         # scale color range from [0, 255] to [0, 1]
-        image_copy=  image_copy/255.0
-            
-        
+        if np.max(image_copy) > 1:
+            image_copy = image_copy / 255.0
+
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-        key_pts_copy = (key_pts_copy - self.mean)/self.std
+        key_pts_copy = (key_pts_copy - self.mean) / self.std
 
 
         return {'image': image_copy, 'keypoints': key_pts_copy}
